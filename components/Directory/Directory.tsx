99815e01feb62390f15ce2a9c6b202bbdbb3373c
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-06-21 00:33:37 +0300

    type fixed in directory

--- components/Directory/Directory.tsx
+++ components/Directory/Directory.tsx
@@ -3,12 +3,13 @@ import styles from "./Directory.module.scss";
 
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
+import Section from "../../models/section";
 const Directory: React.FC = () => {
   const sections = useSelector((state: RootState) => state.directory);
 
   return (
     <div className={styles["directory-menu"]}>
-      {sections.map(section => (
+      {sections.map((section:Section) => (
         <MenuItem key={section.id} section={section} />
       ))}
     </div>
