8cc42cadfe214994d550efe15d9dd75e6bed0466
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-06-21 01:02:13 +0300

    section

--- store/directory-slice.ts
+++ store/directory-slice.ts
@@ -1,8 +1,65 @@
-// @ts-ignore
 import { createSlice } from "@reduxjs/toolkit";
-import sections from "../assets/Sections";
+import Section from "../models/section";
 
-const initialState = sections;
+const initialState: Section[] = [
+  {
+    title: "Girls",
+    imgUrl:
+      "https://images.unsplash.com/photo-1560264589-358fe5a56efd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHRveXMlMjBmb3IlMjBnaXJsc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
+    id: 1,
+  },
+  {
+    title: "Boys",
+    imgUrl:
+      "https://images.unsplash.com/photo-1597422232698-1a27a1289cea?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzR8fHRveXMlMjBmb3IlMjBib3lzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
+    id: 2,
+  },
+  {
+    title: "Action figures",
+    imgUrl:
+      "https://images.unsplash.com/photo-1558507334-57300f59f0bd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHRveXNhY3Rpb24lMjBmaWd1cmVzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
+    id: 3,
+  },
+  {
+    title: "Animals",
+    imgUrl:
+      "https://images.unsplash.com/photo-1603589138334-be34651248b0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dG95cyUyMGFuaW1hbHN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
+    id: 4,
+  },
+  {
+    title: "Cars and RC",
+    imgUrl:
+      "https://images.unsplash.com/photo-1583907388371-7ca9b1bcfbf1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVtb3RlJTIwdG95JTIwY2FyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
+    id: 5,
+  },
+
+  {
+    title: "Creative toys",
+    imgUrl:
+      "https://images.unsplash.com/photo-1516981879613-9f5da904015f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y3JlYXRpdmUlMjB0b3lzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
+    id: 6,
+  },
+  {
+    title: "Dolls",
+    imgUrl:
+      "https://images.unsplash.com/photo-1603362305486-75a25f73051a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZG9sbHMlMjB0b3lzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
+    id: 7,
+  },
+
+  {
+    title: "Games",
+    imgUrl:
+      "https://images.unsplash.com/photo-1611371805429-8b5c1b2c34ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW9ub3BvbHl8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60",
+    id: 8,
+  },
+
+  {
+    title: "Wooden toys",
+    imgUrl:
+      "https://images.unsplash.com/photo-1560859251-d563a49c5e4a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8d29vZGVuJTIwdG95fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
+    id: 9,
+  },
+];
 
 const directorySlice = createSlice({
   name: "directory",
