8ed1b9b23da0c211f3b043357f010a46437db52e
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-07-26 01:48:51 +0200

    stripe

--- pages/checkout/index.tsx
+++ pages/checkout/index.tsx
@@ -44,20 +44,25 @@ const CheckoutPage: React.FC = () => {
           </>
         )}
         {!isEmpty && (
-          <div className={styles.total}>
-            <span>
-              TOTAL : {numberOfItems} piece{numberOfItems > 1 ? "s" : ""}, €
-              {total}
-            </span>
-          </div>
+          <>
+            <div className={styles.total}>
+              <span>
+                TOTAL : {numberOfItems} piece{numberOfItems > 1 ? "s" : ""}, €
+                {total}
+              </span>
+            </div>
+
+            <div className={styles.payment}>
+              <StripeButton price={total} />
+            </div>
+            <div
+              style={{ fontSize: "12px", color: "red", textAlign: "center" }}
+            >
+              <p> Please use test credit card for payments</p>
+              <p>4242 4242 4242 4242 Exp: 01/23 CVC:123</p>
+            </div>
+          </>
         )}
-        <div className={styles.payment}>
-          <StripeButton price={total} />
-        </div>
-        <div style={{ fontSize: "12px", color: "red", textAlign: "center" }}>
-          <p> Please use test credit card for payments</p>
-          <p>4242 4242 4242 4242 Exp: 01/23 CVC:123</p>
-        </div>
       </div>
     </section>
   );
